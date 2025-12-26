2030df374ce45844653cab2899a4ba3d73eef0b0
seg <segnities007@gmail.com>
2025-12-27 01:19:31 +0900

    modify

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,3 +13,33 @@ plugins {
     alias(libs.plugins.dokka)
     alias(libs.plugins.dependencyCheck)
 }
+
+subprojects {
+    apply(plugin = "org.jetbrains.dokka")
+}
+
+dependencies {
+    dokka(projects.composeApp)
+    // Domain
+    dokka(projects.domain.core)
+    dokka(projects.domain.aggregate.task)
+    dokka(projects.domain.aggregate.history)
+    dokka(projects.domain.aggregate.alarm)
+    dokka(projects.domain.aggregate.settings)
+    // Data
+    dokka(projects.data.core)
+    dokka(projects.data.aggregate.task)
+    dokka(projects.data.aggregate.history)
+    dokka(projects.data.aggregate.alarm)
+    dokka(projects.data.aggregate.settings)
+    // Presentation
+    dokka(projects.presentation.core)
+    dokka(projects.presentation.navigation)
+    dokka(projects.presentation.feature.home)
+    dokka(projects.presentation.feature.task)
+    dokka(projects.presentation.feature.history)
+    dokka(projects.presentation.feature.settings)
+    dokka(projects.presentation.feature.management)
+    // DI
+    dokka(projects.di)
+}
